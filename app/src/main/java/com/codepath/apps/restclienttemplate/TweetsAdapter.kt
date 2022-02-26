9c004a2d56935c68a8bad5bd224d0ca634f674d9
Jeffrey Li <lijeffrey02@gmail.com>
2022-02-26 00:53:08 -0500

    finish

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
@@ -30,8 +30,7 @@ class TweetsAdapter (val tweets: ArrayList<Tweet>) : RecyclerView.Adapter<Tweets
 
         holder.tvUserName.text = tweet.user?.name
         holder.tvTweetBody.text = tweet.body
-//        val something = getTimeDifference(tweet.createdAt)
-        holder.tvTime.text = "2m"
+        holder.tvTime.text = getTimeDifference(tweet.createdAt)
 
         Glide.with(holder.itemView).load(tweet.user?.publicImageUrl).into(holder.ivProfileImage)
     }
@@ -54,6 +53,6 @@ class TweetsAdapter (val tweets: ArrayList<Tweet>) : RecyclerView.Adapter<Tweets
         val ivProfileImage = itemView.findViewById<ImageView>(R.id.ivProfileImage)
         val tvUserName = itemView.findViewById<TextView>(R.id.tvUsername)
         val tvTweetBody = itemView.findViewById<TextView>(R.id.tvTweetBody)
-        val tvTime = itemView.findViewById<TextView>(R.id.time)
+        val tvTime = itemView.findViewById<TextView>(R.id.tvTime)
     }
 }
